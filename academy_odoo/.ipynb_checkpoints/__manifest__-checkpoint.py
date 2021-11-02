abf07e7d1612c1cc25321b37f7ed27a2d597cfa3
Lisdrelys <lisdrelys@gmail.com>
2021-11-02 14:57:38 +0000

    Basic Module

---
+++ academy_odoo/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,17 @@
+# -*_ coding:utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app manage training""",
+    'description': """Academy Module to manage training:
+      - Courses
+      - Sessions
+      - Attendees
+     """,
+    'author': 'odoo',
+    'website': 'https://www.odoo.com',
+    'caregory': 'training',
+    'version': '0.1',
+    'depends'; ['base'],
+    'data': [],
+    'demo': [],
+}
